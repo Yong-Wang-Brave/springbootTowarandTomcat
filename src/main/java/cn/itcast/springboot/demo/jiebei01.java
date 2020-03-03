59659c798b2f0ade6a773ab97bb86b06467e1b82
root <a1234567>
2020-03-03 16:30:49 +0800

    提交

--- src/main/java/cn/itcast/springboot/demo/jiebei01.java
+++ src/main/java/cn/itcast/springboot/demo/jiebei01.java
@@ -1,4 +1,13 @@
 package cn.itcast.springboot.demo;
 
 public class jiebei01 {
+
+
+
+    public static void main(String[] args){
+        System.out.println("heiheiddddd");
+        System.out.println("heiheiddddd");
+    }
+
+
 }
